8bd46d036822890860c0e3f5d00ddbfb28d27b2a
Devashish <108848337+Momochor@users.noreply.github.com>
2024-08-06 21:34:18 +0545

    Updated Of pirate question

--- question1.php
+++ question1.php
@@ -3,23 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ONE PIECE</title>
+    <title>Pirate Treasure Map</title>
 </head>
 <body>
 </body>
 <?php
-    $clue = "Find the hidden treasure at the golden island";
-    echo $clue;
-    $arr = explode(" ",$clue);
-    echo "<br>";
-    $i=0;
-    foreach ($arr as $key => $value) {
-        if($value=="golden"){
-            $arr[$i] = "mystic";
-        }
-        $i++;
-    }
-    $result = implode(" ",$arr);
+    $clue = "find the hidden treasure at the golden island";
+    echo $clue."<br>";
+    $find = "golden";
+    $replace = "mystic";
+    $result = str_replace($find,$replace,$clue);
     echo strtoupper($result);
 ?>
 </html>
